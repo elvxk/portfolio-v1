38ab328e82895cb8d2a9a345da6a479efc7b01c6
elvxk <pradiptaramadan9c@gmail.com>
2024-02-26 19:38:20 +0700

    📦 Paket...

---
+++ src/components/Switcher.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { LuMoonStar, LuSun } from "react-icons/lu";
+
+const Switcher = () => {
+  const [theme, setTheme] = useState(() => {
+    const initialTheme = localStorage.getItem("theme");
+    return initialTheme ? initialTheme : "dark";
+  });
+
+  const toggleTheme = () => {
+    setTheme((prev) => {
+      const newTheme = prev === "dark" ? "light" : "dark";
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+
+  const getThemeStorage = () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  };
+
+  useEffect(() => {
+    getThemeStorage();
+    if (theme === "light") {
+      document.documentElement.classList.toggle("dark", true);
+    } else {
+      document.documentElement.classList.toggle("dark", false);
+    }
+  }, [theme]);
+
+  return (
+    <div>
+      <button
+        onClick={toggleTheme}
+        className="text-white bg-slate-700 dark:bg-slate-400 dark:text-slate-800 font-bold text-xl p-2 rounded-full border-2 border-transparent hover:border-violet-500"
+      >
+        {theme === "light" ? <LuSun /> : <LuMoonStar />}
+      </button>
+    </div>
+  );
+};
+export default Switcher;
